b86b7cf7dd89c9381cbf5edfd88bd302c9de7d41
Valentina BM <valentina15bermo@gmail.com>
2026-07-08 21:37:59 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -6,28 +6,50 @@ const area = PI * radius * radius;
 
 const minSpacePerPlant = 0.8
 const startingPlants = 20;
-// let week = 1;
+let week = 1;
 
-// let plantCount = startingPlants * (2 ** week)
+let plantCount = startingPlants * (2 ** week)
 
-// let maxCapacity = area / minSpacePerPlant;
+let maxCapacity = area / minSpacePerPlant;
 
 // plant growth for each week
-// let week = 1;
-// let plantCount = startingPlants * (2 ** week)
-// console.log(`During ${week} there were ${plantCount}`);
+let week = 1;
+let plantCount = startingPlants * (2 ** week)
+console.log(`During ${week} there were ${plantCount}`);
 
-// let week = 2; let plantCount = startingPlants * (2 ** week) 
-// console.log(`During ${week} there were ${plantCount}`); 
+let week = 2; let plantCount = startingPlants * (2 ** week) 
+console.log(`During ${week} there were ${plantCount}`); 
 
-// let week = 3; let plantCount = startingPlants * (2 ** week) 
-// console.log(`During ${week} there were ${plantCount}`);
+let week = 3; let plantCount = startingPlants * (2 ** week) 
+console.log(`During ${week} there were ${plantCount}`);
 
-// Pruned
+// Pruned section
+
 const maxCapacity = area / minSpacePerPlant;
 
-const limit = maxCapacity * 0.8;
+const pruneLimit = maxCapacity * 0.8;
 
-if(plantCount > limit) {
+if(plantCount > pruneLimit) {
+    console.log("Prune the plants");
+}
+
+// monitored section 50% - 80%
+
+const pruneLimit = maxCapacity * 0.8;
+const monitorLimit = maxCapacity * 0.5
+
+if(plantCount > pruneLimit) {
     console.log("Prune the plants")
+} else if (plantCount >= monitorLimit && plantCount <= pruneLimit){
+    console.log("Monitor the plants");
+}
+
+// Planted section less than 50%
+
+if(plantCount > pruneLimit) {
+    console.log("Prune the plants")
+} else if (plantCount >= monitorLimit && plantCount <= pruneLimit){
+    console.log("Monitor the plants");
+} else {
+    console.log("Plant more plants");
 }
